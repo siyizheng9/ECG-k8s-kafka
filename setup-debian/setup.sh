4fc1fb7f6fc34f56545e588c6ffcc7d5419bd7b1
siyi.zheng <siyi.zheng@aalto.fi>
2017-07-26 15:05:40 +0300

    add -h

--- setup-debian/setup.sh
+++ setup-debian/setup.sh
@@ -32,7 +32,7 @@ sudo apt install zsh
 sudo apt install curl
 
 cat >> ~/.zshrc <<EOF
-alias ll='ls -al'
+alias ll='ls -alh'
 EOF
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
