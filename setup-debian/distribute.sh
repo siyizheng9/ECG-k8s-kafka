08af2234425880055ae29d543f0eeff1c21033be
siyi.zheng <siyi.zheng@aalto.fi>
2017-08-02 13:34:02 +0300

    change to setup scripts

---
+++ setup-debian/distribute.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+. ../lib/library.sh
+
+HOST1='192.168.56.101'
+HOST2='192.168.56.102'
+HOST3='192.168.56.103'
+
+CONTENT='setup.sh'
+
+for i in $HOST1 $HOST2 $HOST3
+do
+    print_progress "Test connectivity to $i"
+    nc -z -w 3 $i 22 
+    online=$?
+    if [ $online -eq 0 ]; then
+        print_progress "$i Online"
+        print_progress "rsync files to $i"
+        scp $CONTENT zsy@${i}:~/
+    else
+        print_progress "$i Offline"
+    fi
+done
--- setup-debian/setup.sh
+++ setup-debian/setup.sh
@@ -5,13 +5,13 @@
 # ssh user@123.45.56.78 "mkdir -p ~/.ssh && cat >>  ~/.ssh/authorized_keys"
 
 # install vim
-sudo apt install vim
+sudo apt -y install vim
 
 # install netstat
-sudo apt install net-tools
+sudo apt -y install net-tools
 
 # install tmux
-sudo apt install tmux
+sudo apt -y install tmux
 
 cat >> ~/.vimrc <<EOF
 syntax on
@@ -22,21 +22,20 @@ nnoremap ; :
 EOF
 
 # install docker
-wget https://download.docker.com/linux/debian/dists/stretch/pool/stable/amd64/docker-ce_17.03.2~ce-0~debian-stretch_amd64.deb
-sudo dpkg -i $(ls docker*)
-sudo usermod -aG docker $USER
+# wget https://download.docker.com/linux/debian/dists/stretch/pool/stable/amd64/docker-ce_17.03.2~ce-0~debian-stretch_amd64.deb
+# sudo dpkg -i $(ls docker*)
+# sudo usermod -aG docker $USER
 
 # install and setup zsh
-sudo apt install git
-sudo apt install zsh
-sudo apt install curl
-
-cat >> ~/.zshrc <<EOF
-alias ll='ls -al'
-EOF
+sudo apt -y install git
+sudo apt -y install zsh
+sudo apt -y install curl
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-mkdir ~/kubernetes
+cat >> ~/.zshrc <<EOF
+alias ll='ls -alhG'
+EOF
+# mkdir ~/kubernetes
 
 
