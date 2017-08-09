deab5a1123a3dd7735bfd0a6dbd53635b06a8862
siyi.zheng <siyi.zheng@aalto.fi>
2017-08-09 14:02:59 +0300

    added comment refer

--- setup_network/setup_network.sh
+++ setup_network/setup_network.sh
@@ -3,3 +3,5 @@
 #if not using flannel
 sudo ip route add 10.200.1.0/24 via 10.0.2.13
 sudo ip route add 10.200.0.0/24 via 10.0.2.12
+
+# refer:https://github.com/kelseyhightower/kubernetes-the-hard-way/blob/4d442675ba44c418be02709f61f192b09c4babc9/docs/08-network.md
