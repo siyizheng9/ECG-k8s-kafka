597267f312d7746636a8421790ade12a12d13f51
siyi.zheng <siyi.zheng@aalto.fi>
2017-08-21 10:49:40 +0300

    kafka python consumer and producer

---
+++ kafka/consumer.py
@@ -0,0 +1,30 @@
+#!/bin/env python3
+
+from kafka import KafkaConsumer
+from kafka import TopicPartition
+
+
+def test():
+    topics=['test-basic-with-kafkacat','test']
+    server='kafka-0.broker.kafka.svc.cluster.local'
+
+    consumer = KafkaConsumer(bootstrap_servers=server)
+
+    topic = topics[1]
+    p = consumer.partitions_for_topic(topic)
+    print('partitions for topic %s: %s' % (topic, p))
+
+    available_topics = consumer.topics()
+    print("topics:%s" % (available_topics))
+
+    partition = TopicPartition('test', 0)
+    print(type(partition))
+    consumer.assign([partition])
+    consumer.seek(partition, 100)
+
+    for msg in consumer:
+        print ("partition:%s offset:%s value:%s" % (msg.partition, msg.offset, msg.value))
+
+
+if __name__ == '__main__':
+    test()
---
+++ kafka/producer.py
@@ -0,0 +1,11 @@
+#!/bin/env python3
+
+from kafka import KafkaProducer
+
+topic='test'
+server='kafka-0.broker.kafka.svc.cluster.local'
+
+producer = KafkaProducer(bootstrap_servers=server)
+
+for n in range(100):
+    metadata = producer.send(topic, bytes(n))
