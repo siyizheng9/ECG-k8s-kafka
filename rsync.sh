f896bcd7fe46fa9e3921521d15e693f1d2689573
siyi.zheng <siyi.zheng@aalto.fi>
2017-08-21 20:19:37 +0300

    fixed bug

--- rsync.sh
+++ rsync.sh
@@ -2,7 +2,7 @@
 
 if [ $# -eq 0 ]; then
     rsync -ruv ./kafka/*.py debian-pod:/home/zheng/development/kafka
-    rsync -ruv ./mqtt/*.py debian-pod:/home/zheng/development/mqtt
+    rsync -ruv ./mqtt/python/*.py debian-pod:/home/zheng/development/mqtt
 elif [ $1 == '-d' ]; then
     echo "running in background"
     fswatch -o . | while read f; do ./rsync.sh; done > /dev/null &
