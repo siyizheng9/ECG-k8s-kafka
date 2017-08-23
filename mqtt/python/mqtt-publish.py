a1dc79ee3127520ba4bdb7238fbcca46fbf84d43
siyi.zheng <siyi.zheng@aalto.fi>
2017-08-23 13:44:53 +0300

    added test code

--- mqtt/python/mqtt-publish.py
+++ mqtt/python/mqtt-publish.py
@@ -1,5 +1,29 @@
 #!/bin/env python3
 
-import paho.mqtt.publish as publish
+import paho.mqtt.client as mqtt
+import time
 
-publish.single("paho/test/simple", "payload", hostname="mqtt-svc")
+
+def read_msg():
+    return "message"
+    pass
+
+
+# The callback for when the client recieves a CONNACK response from the server.
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code " + str(rc))
+
+
+client = mqtt.Client()
+client.on_connect = on_connect
+
+client.connect("mqtt-svc", 1883, 60)
+
+client.loop_start()
+
+n = 1
+while True:
+    n += 1
+    msg = read_msg()
+    client.publish("paho/test/simple", msg + str(n))
+    time.sleep(2)
--- mqtt/python/mqtt-subscribe.py
+++ mqtt/python/mqtt-subscribe.py
@@ -1,6 +1,36 @@
 #!/bin/env python3
 
-import paho.mqtt.subscribe as subscribe
+import paho.mqtt.client as mqtt
 
-msg = subscribe.simple("paho/test/simple", hostname="mqtt-svc")
-print("%s %s" % (msg.topic, msg.payload))
+
+def kafka_produce(msg):
+    print("kafka_produce msg: " + msg)
+    pass
+
+
+# The callback for when the client recieves a CONNACK response from the server.
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code " + str(rc))
+
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscritions will be renewed.
+    client.subscribe("paho/test/simple")
+
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    print(msg.topic + " " + str(msg.payload))
+    kafka_produce(str(msg.payload))
+
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect("mqtt-svc", 1883, 60)
+
+# Blocking call that processes network traffic, dispactches callbacks and
+# handles reconnecting.
+# Other loop*() functions are available that given a threaded interface and a
+# manaual interface.
+client.loop_forever()
