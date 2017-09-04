c640c5ff4941a6b8a89bddf210ff7a5155a2f700
siyi.zheng <siyi.zheng@aalto.fi>
2017-09-04 09:53:32 +0300

    add server info to page

--- web/frontend/js/mqtt.js
+++ web/frontend/js/mqtt.js
@@ -1,9 +1,11 @@
 console.log("hello world");
 server = new Object();
-server.hostname = "localhost";
+server.hostname = "192.168.1.101";
 server.port = 30831;
+server.topic = "paho/test/simple"
 // Create a client instance
 client = new Paho.MQTT.Client(server.hostname, Number(server.port), "mqtt_js_client_test");
+writeServerInfo(server);
 
 // set callback handlers
 client.onConnectionLost = onConnectionLost;
@@ -17,22 +19,24 @@ client.connect({onSuccess:onConnect});
 function onConnect() {
   // Once a connection has been made, make a subscription and send a message.
   console.log("onConnect");
-  client.subscribe("paho/test/simple");
+  updateStatus("Connected");
+  client.subscribe(server.topic);
   message = new Paho.MQTT.Message("Hello");
-  message.destinationName = "paho/test/simple";
+  message.destinationName = server.topic;
   client.send(message);
 }
 
 // called when the client loses its connection
 function onConnectionLost(responseObject) {
+  updateStatus("Connection Lost");
   if (responseObject.errorCode !== 0) {
-    console.log("onConnectionLost:"+responseObject.errorMessage);
+    console.log("onConnectionLost:" + responseObject.errorMessage);
   }
 }
 
 // called when a message arrives
 function onMessageArrived(message) {
-  console.log("onMessageArrived:"+message.payloadString);
+  console.log("onMessageArrived:" + message.payloadString);
   writeMessage(message.payloadString);
 }
 
@@ -47,4 +51,24 @@ function writeMessage(message) {
     list.appendChild(item);
 
     list.scrollTop = list.scrollHeight;
+}
+
+// write server info
+function writeServerInfo(server) {
+  el_host = document.getElementById("server_host");
+  el_host.innerHTML = "host:" + server.hostname;
+
+  el_port = document.getElementById("server_port");
+  el_port.innerHTML = "port:" + server.port;
+
+  el_topic = document.getElementById("server_topic");
+  el_topic.innerHTML = "topic:" + server.topic;
+
+}
+
+// update connection status
+function updateStatus(status) {
+  el_status = document.getElementById("server_status");
+  el_status.innerHTML = "status:" + status;
+
 }
