03a9e1f12a0f26e7cac127827e1d619de169e1a3
siyi.zheng <siyi.zheng@aalto.fi>
2017-09-04 11:07:47 +0300

    add connect btn logic

--- web/frontend/js/mqtt.js
+++ web/frontend/js/mqtt.js
@@ -1,37 +1,62 @@
 console.log("hello world");
+
+// server info
 server = new Object();
 server.hostname = "192.168.1.101";
 server.port = 30831;
 server.topic = "paho/test/simple"
-// Create a client instance
+
 client = new Paho.MQTT.Client(server.hostname, Number(server.port), "mqtt_js_client_test");
-writeServerInfo(server);
 
-// set callback handlers
-client.onConnectionLost = onConnectionLost;
-client.onMessageArrived = onMessageArrived;
+document.getElementById("connect_btn").addEventListener("click", OnconnectBtnClick);
 
-// connect the client
-client.connect({onSuccess:onConnect});
+isConnected = false;
 
+function OnconnectBtnClick() {
+  if (isConnected == false)
+    ConnectToServer(client, server);
+  else
+    disConnect(client);
+}
+
+// connect to the server 
+function ConnectToServer(client, server) {
+  console.log("connect btn clicked");
+  // Create a client instance
+  writeServerInfo(server);
+
+  // set callback handlers
+  client.onConnectionLost = onConnectionLost;
+  client.onMessageArrived = onMessageArrived;
+
+  // connect the client
+  client.connect({onSuccess:onConnect});
+}
 
 // called when the client connects
 function onConnect() {
   // Once a connection has been made, make a subscription and send a message.
   console.log("onConnect");
-  updateStatus("Connected");
+  updateStatus(true);
   client.subscribe(server.topic);
   message = new Paho.MQTT.Message("Hello");
   message.destinationName = server.topic;
   client.send(message);
+  isConnected = true;
+}
+
+function disConnect(client) {
+  console.log("disconnect");
+  client.disconnect();
 }
 
 // called when the client loses its connection
 function onConnectionLost(responseObject) {
-  updateStatus("Connection Lost");
+  updateStatus(false);
   if (responseObject.errorCode !== 0) {
     console.log("onConnectionLost:" + responseObject.errorMessage);
   }
+  isConnected = false;
 }
 
 // called when a message arrives
@@ -68,7 +93,15 @@ function writeServerInfo(server) {
 
 // update connection status
 function updateStatus(status) {
-  el_status = document.getElementById("server_status");
-  el_status.innerHTML = "status: " + status;
+  if (status == true) {
+    msg = "Connected";
+    document.getElementById("connect_btn").innerText = "Disconnect"
+  }
+  else {
+    msg = "Connection Lost";
+    document.getElementById("connect_btn").innerText = "Connect"
+  }
 
+  el_status = document.getElementById("server_status");
+  el_status.innerHTML = "status: " + msg;
 }
