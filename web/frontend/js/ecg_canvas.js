5d78be460f44caecd129efdbf6fefa1b4bbb8747
siyi.zheng <siyi.zheng@aalto.fi>
2017-09-04 14:30:33 +0300

    test canvas with sample data

--- web/frontend/js/ecg_canvas.js
+++ web/frontend/js/ecg_canvas.js
@@ -2,35 +2,44 @@ demo = document.getElementById("demo")
 var ctx = demo.getContext('2d'),
 w = demo.width,
 h = demo.height,
-px = 0, opx = 0, speed = 3,
+px = 0, opx = 0, speed = 1,
 py = h * 0.8, opy = py,
-scanBarWidth = 20;
+scanBarWidth = 15;
 
 ctx.strokeStyle = '#00bd00';
-ctx.lineWidth = 3;
+ctx.lineWidth = 1;
 
-demo.onmousemove = function(e) {
-var r = demo.getBoundingClientRect();
-py = e.clientY - r.top;
+var i = 0;
+var data = ecg_data;
+
+function updatePYval(){
+    //py = (parseInt(Math.round(data[i])/h));
+    py = (parseInt(data[i].data) / 1000 * h);
+    i++;
+    if(i == data.length - 1)
+        i = 0;
 }
+
 loop();
 
 function loop() {
 
-px += speed;
+    updatePYval();
 
-ctx.clearRect(px,0, scanBarWidth, h);
-ctx.beginPath();
-ctx.moveTo(opx, opy);
-ctx.lineTo(px, py);
-ctx.stroke();
+    px += speed;
 
-opx = px;
-opy = py;
+    ctx.clearRect(px, 0, scanBarWidth, h);
+    ctx.beginPath();
+    ctx.moveTo(opx, opy);
+    ctx.lineTo(px, py);
+    ctx.stroke();
 
-if (opx > w) {
-    px = opx = -speed;
-}
+    opx = px;
+    opy = py;
 
-requestAnimationFrame(loop);
+    if (opx > w) {
+        px = opx = -speed;
+    }
+
+    requestAnimationFrame(loop);
 }
