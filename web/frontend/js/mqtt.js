50ee0c8dd04967bd14b5a05f4168608a8f6c0ee5
siyi.zheng <siyi.zheng@aalto.fi>
2017-09-04 21:53:07 +0300

    add bootstrap style

--- web/frontend/js/mqtt.js
+++ web/frontend/js/mqtt.js
@@ -1,12 +1,13 @@
 console.log("hello world");
 
-// server info
-server = new Object();
-server.hostname = "192.168.1.101";
-server.port = 30831;
-server.topic = "paho/test/simple"
+// connection info
+var connection = new Object();
+connection.hostname = "192.168.1.101";
+connection.port = 30831;
+connection.topic = "paho/test/simple"
+connection.client_id = "mqtt_js_client_test"
 
-client = new Paho.MQTT.Client(server.hostname, Number(server.port), "mqtt_js_client_test");
+writeDefaultConnectionInfo(connection);
 
 document.getElementById("connect_btn").addEventListener("click", OnconnectBtnClick);
 document.getElementById("msg_log_btn").addEventListener("click", OnMsgLogBtnClick);
@@ -15,8 +16,10 @@ isConnected = false;
 isLogMsg = true;
 
 function OnconnectBtnClick() {
+  getConnectionInfo();
+  client = new Paho.MQTT.Client(connection.hostname, Number(connection.port), connection.client_id);
   if (isConnected == false)
-    ConnectToServer(client, server);
+    ConnectToServer(client, connection);
   else
     disConnect(client);
 }
@@ -24,17 +27,17 @@ function OnconnectBtnClick() {
 function OnMsgLogBtnClick() {
   btn_item = document.getElementById("msg_log_btn")
   if (isLogMsg == true) {
-    btn_item.innerText = "disabled"
+    btn_item.innerText = "disabled";
+    btn_item.className = "label label-warning";
   } else {
-    btn_item.innerText = "enabled"
+    btn_item.innerText = "enabled"; 
+    btn_item.className = "label label-success";
   }
   isLogMsg = !isLogMsg;
 }
-// connect to the server 
-function ConnectToServer(client, server) {
+// connect to the server
+function ConnectToServer(client, connection) {
   console.log("connect btn clicked");
-  // Create a client instance
-  writeServerInfo(server);
 
   // set callback handlers
   client.onConnectionLost = onConnectionLost;
@@ -95,16 +98,28 @@ function writeMessage(message) {
     list.scrollTop = list.scrollHeight;
 }
 
-// write server info
-function writeServerInfo(server) {
-  el_host = document.getElementById("server_host");
-  el_host.innerHTML = "host: " + server.hostname;
 
-  el_port = document.getElementById("server_port");
-  el_port.innerHTML = "port: " + server.port;
+function writeDefaultConnectionInfo(connection) {
+  document.getElementById("server_host").value = connection.hostname;
 
-  el_topic = document.getElementById("server_topic");
-  el_topic.innerHTML = "topic: " + server.topic;
+  document.getElementById("server_port").value = connection.port;
+
+  document.getElementById("server_topic").value = connection.topic;
+
+  document.getElementById("client_id").value = connection.client_id;
+
+}
+
+function getConnectionInfo() {
+
+  connection.hostname = document.getElementById("server_host").value;
+  console.log("host:" + connection.hostname);
+
+  connection.port = document.getElementById("server_port").value;
+
+  connection.topic = document.getElementById("server_topic").value;
+
+  connection.client_id = document.getElementById("client_id").value;
 
 }
 
@@ -112,13 +127,24 @@ function writeServerInfo(server) {
 function updateStatus(status) {
   if (status == true) {
     msg = "Connected";
+    clsname = "label label-success";
     document.getElementById("connect_btn").innerText = "Disconnect"
+    document.getElementById("server_host").disabled = true;
+    document.getElementById("server_port").disabled = true;
+    document.getElementById("server_topic").disabled = true;
+    document.getElementById("client_id").disabled = true;
   }
   else {
     msg = "Connection Lost";
+    clsname = "label label-warning";
     document.getElementById("connect_btn").innerText = "Connect"
+    document.getElementById("server_host").disabled = false;
+    document.getElementById("server_port").disabled = false;
+    document.getElementById("server_topic").disabled = false;
+    document.getElementById("client_id").disabled = false;
   }
 
   el_status = document.getElementById("server_status");
+  el_status.className = clsname;
   el_status.innerHTML = "status: " + msg;
 }
