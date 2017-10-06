4a286595fe25a5dbb99d9c817b8f9c596980a512
siyi.zheng <siyi.zheng@aalto.fi>
2017-10-06 20:02:11 +0300

    add variables

--- rsync.sh
+++ rsync.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+#HOST='debain-pod'
+HOST='r-debian-1'
+DEST='~/Development/'
 
 if [ $# -eq 0 ]; then
-    rsync -ruv ./kafka/*.py debian-pod:/home/zheng/development/kafka
-    rsync -ruv ./mqtt/python/*.py debian-pod:/home/zheng/development/mqtt
+    rsync -ruv ./kafka/*.py $HOST:$DEST/kafka
+    rsync -ruv ./mqtt/python/*.py $HOST:$DEST/mqtt
 elif [ $1 == '-d' ]; then
     echo "running in background"
     fswatch -o . | while read f; do ./rsync.sh; done > /dev/null &
