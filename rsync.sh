a7777f46352d07431df306e916d6c3dcfcee10ca
siyi.zheng <siyi.zheng@aalto.fi>
2017-10-06 21:47:29 +0300

    update synchronized files

--- rsync.sh
+++ rsync.sh
@@ -5,7 +5,7 @@ DEST='~/Development/'
 
 if [ $# -eq 0 ]; then
     rsync -ruv ./kafka/*.py $HOST:$DEST/kafka
-    rsync -ruv ./mqtt/python/*.py $HOST:$DEST/mqtt
+    rsync -ruv ./mqtt/ $HOST:$DEST/mqtt/
 elif [ $1 == '-d' ]; then
     echo "running in background"
     fswatch -o . | while read f; do ./rsync.sh; done > /dev/null &
