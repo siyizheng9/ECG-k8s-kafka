29c3668f8241f82c0c8de35883815121a0b35109
siyi.zheng <siyi.zheng@aalto.fi>
2017-10-07 20:06:37 +0300

    check latest offset

--- kafka/consumer.py
+++ kafka/consumer.py
@@ -4,9 +4,11 @@ from kafka import KafkaConsumer
 from kafka import TopicPartition
 
 
+server = 'kafka-0.broker.kafka.svc.cluster.local'
+
+
 def test():
     topics = ['test-basic-with-kafkacat', 'test']
-    server = 'kafka-0.broker.kafka.svc.cluster.local'
 
     consumer = KafkaConsumer(bootstrap_servers=server)
 
@@ -27,5 +29,19 @@ def test():
               (msg.partition, msg.offset, msg.value))
 
 
+def get_last_offset():
+    consumer = KafkaConsumer(bootstrap_servers=server)
+    topic = 'mqtt-test'
+    partition = TopicPartition(topic, 0)
+    consumer.assign([partition])
+    offset = consumer.end_offsets([partition])
+    print("latest offset:%s" % (offset))
+    # consumer.seek_to_end()
+    # msg = next(consumer)
+    # print("partition:%s offset:%s value:%s" %
+    #       (msg.partition, msg.offset, msg.value))
+
+
 if __name__ == '__main__':
-    test()
+    get_last_offset()
+#    test()
