be82bfd4bff9fe0390dcd2fdf4f903bd4f26a2df
siyi.zheng <siyi.zheng@aalto.fi>
2017-10-07 20:53:43 +0300

    more verbose output

--- mqtt/python/mqtt-publish.py
+++ mqtt/python/mqtt-publish.py
@@ -22,13 +22,22 @@ csvfile = open(Config.path_sample_data, 'r')
 
 datareader = csv.reader(csvfile)
 
+count = 0
+start_time = time.time()
 for i in range(int(Config.repeat_test)):
     print('Publishing to topic:', Config.mqtt_topic, 'round:', i)
     for data in datareader:
         msg = data[0] + ',' + data[1]
         client.publish(Config.mqtt_topic, msg)
-        print(msg)
+        # print(msg)
+        count += 1
         time.sleep(int(Config.time_interval))
     csvfile.seek(0)
 
+elapsed_time = time.time() - start_time
 csvfile.close()
+
+print('MQTT_PUB_TIME_INTERVAL:%s' % Config.time_interval,
+      'MQTT_REPEAT_TEST:%s' % Config.repeat_test, sep='\n')
+print('count of message sent:', count)
+print('elapsed time:', elapsed_time)
