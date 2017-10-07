16b63c2f20927ebdb3568e8fa8e11dd6253ec6e4
siyi.zheng <siyi.zheng@aalto.fi>
2017-10-07 20:07:25 +0300

    check message received

--- mqtt/python/mqtt-subscribe.py
+++ mqtt/python/mqtt-subscribe.py
@@ -2,27 +2,29 @@
 
 import paho.mqtt.client as mqtt
 from Config import Config
+import time
 
 
-def kafka_produce(msg):
-    print("kafka_produce msg: " + msg)
-    pass
-
-
+topic = 'ecg/test/+/data'
+count = 0
+# topic = Config.mqtt_topic
 # The callback for when the client recieves a CONNACK response from the server.
+
+
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code " + str(rc))
 
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscritions will be renewed.
-    print('subscribe to the topic:', Config.mqtt_topic)
-    client.subscribe(Config.mqtt_topic)
+    print('subscribe to the topic:', topic)
+    client.subscribe(topic)
 
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
-    print(msg.topic + " " + str(msg.payload))
-    kafka_produce(str(msg.payload))
+    # print(msg.topic + " " + str(msg.payload))
+    global count
+    count += 1
 
 
 client = mqtt.Client()
@@ -35,4 +37,7 @@ client.connect(Config.mqtt_server, int(Config.mqtt_port), 60)
 # handles reconnecting.
 # Other loop*() functions are available that given a threaded interface and a
 # manaual interface.
-client.loop_forever()
+client.loop_start()
+while True:
+    time.sleep(3)
+    print('message received count:', count)
