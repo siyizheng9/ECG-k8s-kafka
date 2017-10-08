c517b1c19bbacb98943aaa530e5495d60fa7c107
siyi.zheng <siyi.zheng@aalto.fi>
2017-10-08 20:35:42 +0300

    add comments

--- scripts/test/debian-1/check_kafka_db.sh
+++ scripts/test/debian-1/check_kafka_db.sh
@@ -18,3 +18,12 @@ kubectl exec mongo-deployment-1281015340-k22ld --  mongo kafkaconnect --eval 'db
 
 check_kafka_offset
 check_db
+
+# kafka delete topic
+# kubectl -n kafka exec kafka-0 -- bin/kafka-topics.sh --zookeeper zookeeper --delete --topic mqtt-test
+
+# kafka create topic
+# kubectl -n kafka exec kafka-0 -- bin/kafka-topics.sh --create --zookeeper zookeeper --replication-factor 3 --partitions 1 --topic mqtt-test
+
+# mongodb drop collection
+# kubectl exec mongo-deployment-1281015340-k22ld --  mongo kafkaconnect --eval 'db.kafkatopic.drop()'
