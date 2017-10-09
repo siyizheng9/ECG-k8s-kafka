67879b8937f485627e6715a558e8d60bdafd6a2e
siyi.zheng <siyi.zheng@aalto.fi>
2017-10-09 16:14:44 +0300

    add loop

--- scripts/test/debian-1/repeat_test.sh
+++ scripts/test/debian-1/repeat_test.sh
@@ -9,8 +9,18 @@ export MQTT_PUB_TIME_INTERVAL=$2
 
 for i in {1..10}
 do
+    echo -n 'time interval:' $2 ' '
+    echo 'round:' $i
     ./mqtt_test.sh >> r_${MQTT_REPEAT_TEST}_t_${MQTT_PUB_TIME_INTERVAL}.log
 done
 }
 
-test 1 0
+# test 1 0
+
+interval_ary="0 0.001 0.0005"
+
+for interval in $interval_ary; do
+
+    test 1 $interval
+
+done
